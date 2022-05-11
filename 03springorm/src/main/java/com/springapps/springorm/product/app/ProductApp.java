7b414c4a571fc80d10c3c56d1f50ffb3d86c8ddb
Your Name <you@example.com>
2022-05-11 22:06:01 +0530

    committed

---
+++ 03springorm/src/main/java/com/springapps/springorm/product/app/ProductApp.java
@@ -0,0 +1,29 @@
+package com.springapps.springorm.product.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springapps.springorm.product.dao.ProductDao;
+import com.springapps.springorm.product.entity.Product;
+
+
+public class ProductApp 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("com/springapps/springorm/product/config/config.xml");
+		ProductDao productdao = (ProductDao) context.getBean("productDaoImpl");
+		
+		createProduct(productdao);
+    }
+
+	private static void createProduct(ProductDao productdao) {
+		Product product = new Product();
+		product.setId(1);
+		product.setName("iWatch");
+		product.setDescription("best watch");
+		product.setPrice(50000);
+		int result = productdao.create(product);
+		System.out.println("number of rows inserted:" + result);
+	}
+}
